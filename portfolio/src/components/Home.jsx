ca288aaf2c5464e83a49078113121c2e9b0aa668
Elika-pvt-lim <sabnischinmay31@gmail.com>
2024-08-22 11:22:08 +0530

    tp

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import bgImg from "../assets/Home-bg.jpg";
+import bgImg from "../assets/home-bg.jpg";
 import "../css/home.css";
 import githubIcon from "../assets/logo.png";
 import instgramIcon from "../assets/instagram.png";
