0a0314eb1a9045b839acd811be7daa1f69ba7515
Chinmay Sabnis <113910809+Schinmay31@users.noreply.github.com>
2024-08-22 11:34:29 +0530

    Update Home.jsx

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import bgImg1 from "../assets/Home-bg.jpg";
+import bgImg1 from "../assets/home-bg.jpg";
 import "../css/home.css";
 import githubIcon from "../assets/logo.png";
 import instgramIcon from "../assets/instagram.png";
